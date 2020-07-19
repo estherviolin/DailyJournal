ccbb44a7dec96a5eb37e60161d93ce39a6fb6e99
estherviolin <esthernaomi.violin@gmail.com>
2020-07-19 09:56:18 -0500

    added mood, date, concept label

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -5,10 +5,10 @@
 export const JournalEntryComponent = (entry) => {
     return `
         <section id="entry--${entry.id}" class="journalEntry">
-            <p class="journalDate">${entry.date}</p>
-            <p class="journalConcept">${entry.concept}</p>
+            <p class="journalDate">Date: ${entry.date}</p>
+            <p class="journalConcept">Concept(s): ${entry.concept}</p>
             <p class="journalStuff">${entry.entry}</p>
-            <p class="journalMood">${entry.mood}</p>
+            <p class="journalMood">Mood: ${entry.mood}</p>
             <button type="button" class="delete">Edit</button>
             <button type="button" class="edit">Delete</button>
         </section>
